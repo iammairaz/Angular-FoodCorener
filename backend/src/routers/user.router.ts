b35b3e6b65713b6276fa3e4a02e6c3d180b4e91a
iammairaz <merajmd457@gmail.com>
2022-12-29 11:31:21 +0530

    New files

--- backend/src/routers/user.router.ts
+++ backend/src/routers/user.router.ts
@@ -24,9 +24,9 @@ router.get("/seed",asyncHandler(
 router.post("/login", asyncHandler(
     async (req, res) => {
       const {email, password} = req.body;
-      const user = await UserModel.findOne({email , password});
+      const user = await UserModel.findOne({email});
       /* const user = sample_users.find(user => user.email === email && user.password === password); */
-      if(user) {
+      if(user && (await bcrypt.compare(password,user.password))) {
         res.send(generateTokenReponse(user));
        }
        else{
